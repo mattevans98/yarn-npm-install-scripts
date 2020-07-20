081ed4917d6854ad1c0f9dc579fc2b4c026abd96
Matt Evans <mattevans98@gmail.com>
2020-07-20 16:21:27 -0500

    Removed eslint installation

--- npm-install-react.sh
+++ npm-install-react.sh
@@ -1,3 +1,3 @@
 npm install --save antd
-npm install --save-dev eslint @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint-config-prettier eslint-plugin-import eslint-plugin-prettier prettier eslint-plugin-react-hooks eslint-plugin-react nodemon
+npm install --save-dev @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint-config-prettier eslint-plugin-import eslint-plugin-prettier prettier eslint-plugin-react-hooks eslint-plugin-react nodemon
 echo 'NPM installation script finished executing! :D'
