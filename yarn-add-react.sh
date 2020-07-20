2137b966f40ca4204631ab8aba497de592e486ca
Matt Evans <mattevans98@gmail.com>
2020-07-20 15:17:42 -0500

    Initial commit

---
+++ yarn-add-react.sh
@@ -0,0 +1,3 @@
+yarn add antd
+yarn add -D eslint @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint-config-prettier eslint-plugin-import eslint-plugin-prettier prettier eslint-plugin-react-hooks eslint-plugin-react nodemon
+echo 'Yarn installation script finished executing! :D'
