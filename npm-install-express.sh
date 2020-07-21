29e64af18e1c195cc59258e49317e4acb966dcf4
Matt Evans <matthew.e.evans@centene.com>
2020-07-21 12:44:18 -0500

    Added lines to copy to package.json

--- npm-install-express.sh
+++ npm-install-express.sh
@@ -7,4 +7,26 @@ destDir=$(pwd)
 cp "${linterDir}/.eslintrc.js" "${linterDir}/.prettierrc.js" "${linterDir}/tsconfig.json" "$destDir"
 git add .eslintrc.js
 git add .prettierrc.js
-echo 'Config files copied and added to git. Have fun!'
+echo 'Config files copied and added to git. You should now add these options to package.json'
+echo "
+\"scripts\": {
+  \"start\": \"node --inspect=5858 -r ts-node/register src/App.ts\",
+  \"start:watch\": \"nodemon\",
+  \"build\": \"tsc\"
+},
+\"nodemonConfig\": {
+  {
+  \"ignore\": [
+      \"**/*.test.ts\",
+      \"**/*.spec.ts\",
+      \".git\",
+      \"node_modules\"
+    ],
+    \"watch\": [
+      \"src\"
+    ],
+    \"exec\": \"npm start\",
+    \"ext\": \"ts\"
+  }
+}
+"
