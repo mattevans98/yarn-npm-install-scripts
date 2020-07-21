cea700fee4ddb18caea7e358d1d2dc5860d2c1d8
Matt Evans <matthew.e.evans@centene.com>
2020-07-21 14:39:29 -0500

    Finished with react and express installers

--- express-installers/express-skeleton.sh
+++ express-installers/express-skeleton.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+chmod +x "${INSTALL_SCRIPTS_DIR}/utils/grant-permissions.sh"
+"${INSTALL_SCRIPTS_DIR}/utils/grant-permissions.sh"
 echo 'Creating project directory structure...'
 mkdir -p src/models src/tests
 touch src/App.ts src/models/App.model.ts src/tests/App.test.ts
 echo 'Would you like to use Yarn? (y/n)'
 read yarnChoice
 wasSuccessful=false
-if [ "$yarnChoice" == 'y'] || [ "$yarnChoice" == 'Y']
+if [ "$yarnChoice" == 'y' ] || [ "$yarnChoice" == 'Y' ]
 then
   "${INSTALL_SCRIPTS_DIR}/express-installers/yarn-add-express.sh"
   wasSuccessful=true
-elif [ "$yarnChoice" == 'n' ] || [ "$yarnChoice" == 'N']
+elif [ "$yarnChoice" == 'n' ] || [ "$yarnChoice" == 'N' ]
 then
   "${INSTALL_SCRIPTS_DIR}/express-installers/npm-install-express.sh"
   wasSuccessful=true
